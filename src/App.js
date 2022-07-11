477785de20b9c44bab97cfb5de913286fa6e1a60
Marin <marin.knezevic1et2@gmail.com>
2022-07-11 19:07:21 +0200

    something something nešto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Todo from "./components/Todo";
 import React from "react";
-
+//although code looks like html behind the scenes it transforms into standard js
 function App() {
-  return <div> Hello motherfuckers! </div>;
+  return (
+    <div>
+      <h1>To do list</h1>
+      <Todo />
+    </div>
+  );
 }
 
+//exporting means that we make certain function available outside of this file :)
 export default App;
