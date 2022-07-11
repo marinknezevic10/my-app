950aa20669dd7b680b75461acce7212df47ce0dd
Marin <marin.knezevic1et2@gmail.com>
2022-07-11 19:07:36 +0200

    something something nešto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 //index.js is a starting point of our application
+//code written here will be the first code executed in the browser
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//with this code we are telling tthat we wanna render our app html element into this element with root id
+const root = ReactDOM.createRoot(document.getElementById("root")); //root element is displayed in index.html file
 root.render(<App />);
