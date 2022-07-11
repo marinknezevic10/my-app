1bc3425fbacba9cd61e29f86a4401dad2094a0c5
Marin <marin.knezevic1et2@gmail.com>
2022-07-11 19:55:55 +0200

    handling events building delete btn

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "../index.css";
 
+//ex. className="card" -> className = key, card=attribute, key->attribute
+//učio već na phpu key->attribute fokusiraj se :)
 //sending parametar props on todo function
 //curly braces {} executes js code
 function Todo(props) {
+  function Delete() {}
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={Delete}>
+          DELETE
+        </button>
       </div>
     </div>
   );
 }
 
+//onClick we are telling react that we want to react on a click element {} should contain function that would be executed when the click occurs
 export default Todo;
