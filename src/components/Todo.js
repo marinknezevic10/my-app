1f95772fdcca409d6f54c92acf84eecb61f3bf85
Marin <marin.knezevic1et2@gmail.com>
2022-07-11 20:19:55 +0200

    starting to use useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../index.css";
+import { useState } from "react";
 
+//with usestate we register differents states which we want to import in our app and react will react to changes in the state and allow us to render different output
+//state allows us to render different output depending on which state is active
+//usestate is a hook and can only be used in component functions
 //ex. className="card" -> className = key, card=attribute, key->attribute
 //učio već na phpu key->attribute fokusiraj se :)
 //sending parametar props on todo function
 //curly braces {} executes js code
 function Todo(props) {
+  useState();
+
   function Delete() {}
 
   return (
