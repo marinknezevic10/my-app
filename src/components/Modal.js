56ce6c28c885e40f54aa4870337be35961c74933
Marin <marin.knezevic1et2@gmail.com>
2022-07-12 13:30:41 +0200

    event props working with confirm and cancel buttons and fuctions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,24 @@
 //modal function which displays div where we can decide if we want to delete an object or not after pressing a delete button
 import React from "react";
 
-function Modal() {
+function Modal(props) {
+  function Cancel() {
+    props.onCancel();
+  }
+
+  function Confirm() {
+    props.onConfirm();
+  }
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={Cancel}>
+        Cancel
+      </button>
+      <button className="btn" onClick={Confirm}>
+        Confirm
+      </button>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,6 +8,7 @@ import Backdrop from "./Backdrop";
 //state allows us to render different output depending on which state is active
 //usestate is a hook and can only be called in component functions
 //usestate always return an array with two elements
+//usestate is important for changing on what we see on the screen(for adding or removing elements,for changing texts...)
 //we can store those two elements in a constant or we use array destructuring which is js feature that stores those two elements in different constants
 
 //ex. className="card" -> className = key, card=attribute, key->attribute
@@ -18,6 +19,9 @@ import Backdrop from "./Backdrop";
 //we use this {modalIsOpen ? <Modal/> : null} because we want to display this function only if its clicked otherwise it will be shown all the time
 //if the modalisopen is true I render the modal if its not I dont render anything
 //{modalIsOpen && <Modal/>} IF BOTH CONDITIONS ARE TRUE THEN SECOND VALUE WILL BE RENDERED
+
+//props are important for building reusable components
+
 function Todo(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -38,7 +42,7 @@ function Todo(props) {
           DELETE
         </button>
       </div>
-      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Modal onCancel={closeModal} onConfirm={closeModal} />}
       {modalIsOpen && <Backdrop onClick={closeModal} />}
     </div>
   );
